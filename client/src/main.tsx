f46eeb3a352df454673cddde9f89d52b32fce730
ElinEunjung <eunjungp615@gmail.com>
2024-10-19 14:43:34 +0200

    diverse changes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Application } from "./componants/app/application";
 
-const root = createRoot(document.getElementById("root"));
-root.render(<h1>Hello Class</h1>);
+const root = createRoot(document.getElementById("root")!);
+root.render(<Application />);
